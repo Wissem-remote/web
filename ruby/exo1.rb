170bdb8c2ecf497ea9c07c2b574273fae63e806a
rezki wissem <wissemrezki@gmail.com>
2021-03-11 18:06:51 +0100

    Ruby Exo1

--- ruby/exo1.rb
+++ ruby/exo1.rb
@@ -6,8 +6,14 @@ nb = gets.to_i
 if  nb <= 0
     puts "Desoler votre chiffre est inferieur ou egale à zero "
 elsif nb > 0
-    wis = "#" * nb
+    n = 0
+    x = nb
+     while n < nb
+        n= n + 1
+    wis = " "*x +"#" * n
     puts wis
+    x = x-1
+     end
 else 
     puts "votre contenue ne correspont pas"
 end
