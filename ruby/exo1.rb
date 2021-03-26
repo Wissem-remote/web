acd1e528cd09e0559fe19b5c5553a4a39ab5390f
rezki wissem <wissemrezki@gmail.com>
2021-03-26 16:17:15 +0100

    Ruby Exo Escalier

--- ruby/exo1.rb
+++ ruby/exo1.rb
@@ -1,19 +1,13 @@
-puts "Entrez votre chiffre"
-nb = gets.to_i
+
+#!/usr/bin/ruby
+puts = ARGV
+nb = ARGV.first.to_i
 
 
-
-if  nb <= 0
-    puts "Desoler votre chiffre est inferieur ou egale à zero "
-elsif nb > 0
     n = 0
-    x = nb
+    
      while n < nb
         n= n + 1
-    wis = " "*x +"#" * n
-    puts wis
-    x = x-1
-     end
-else 
-    puts "votre contenue ne correspont pas"
-end
+    wis = "#" * n
+    puts wis.rjust(nb)
+      end
