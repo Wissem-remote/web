7009e45580aeb0f1dcaca7cb4171d81f80d212f2
rezki wissem <wissemrezki@gmail.com>
2021-03-30 12:58:02 +0200

    Ruby Escalier Plus

--- ruby/exo1.rb
+++ ruby/exo1.rb
@@ -1,13 +1,14 @@
 
 #!/usr/bin/ruby
 
-nb = ARGV.first.to_i
+nb = ARGV
+
+(nb.count > 1) ? (puts 'desoler il y a tros argument') : (arg = nb.first.to_i)
+
+case arg
+when 0 then puts "c'est un carractaire"
+when 1..30 then 1.upto(arg){ |n| printf "%#{arg+arg+1}s","# "*n +"\n" }
+when 30.. then puts "desoler votre chiffre est tros haut"
+end
 
 
-    n = 0
-    
-     while n < nb
-        n= n + 1
-    wis = "#" * n
-    puts wis.rjust(nb)
-      end
