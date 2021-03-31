d8cd3982cefabac5fd5c38d869a0683530cc438b
rezki wissem <wissemrezki@gmail.com>
2021-03-31 22:51:04 +0200

    Ruby Plus Case

--- ruby/exo1.rb
+++ ruby/exo1.rb
@@ -1,10 +1,10 @@
 
-
-
 nb = ARGV
 
-(nb.count > 1) ? (puts 'desoler il y a tros argument') : (arg = nb.first.to_i)
-
+case nb.count
+when 1 then arg = nb.first.to_i
+when 2.. then puts 'desoler il y a tros argument'
+end 
 case arg
 when 0 then puts "c'est un carractaire"
 when 1..30 then 1.upto(arg){ |n| printf "%#{arg+arg+1}s","# "*n +"\n" }
