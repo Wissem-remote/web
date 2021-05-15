e697abb6ef069755a319a468212baac949f79d04
rezki wissem <wissemrezki@gmail.com>
2021-05-15 19:34:24 +0200

    Ruby Rectangle plus

--- ruby/exo5.rb
+++ ruby/exo5.rb
@@ -8,25 +8,19 @@ ARGF.each_line {|line|(c == ARGF.filename) ? (a += line) : (b += line)}
 
 a = a.scan(/./)
 b = b.split(' ')
-o = 1
+i = 1
 
-
-
-i = 0
 b.each {|x| 
 lines = b.index(x)
 
 
 
     x.scan(/./){ |z|
-    (i >= 5) ? (i=0) : (i += 1)
-     if  a[o] == z
-       puts "ligne #{lines} cologne #{i}"
-        o += 1 
-        
+    if  a[i] == z
+    puts "ligne #{lines} cologne #{x.index(z)}"
+    i += 1 
     end
     }
-  
 
 
 }
